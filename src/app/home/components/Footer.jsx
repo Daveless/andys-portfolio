7c433fe7a94f7b01e537c690ecb58d41e478c9ba
daveless <44807207+Daveless@users.noreply.github.com>
2024-06-01 21:00:35 -0500

    mobile version ready

--- src/app/home/components/Footer.jsx
+++ src/app/home/components/Footer.jsx
@@ -16,31 +16,31 @@ const Footer = () => {
   return (
     <div className='w-full bg-portfolio-gray max-w-6xl mx-auto pb-5 pt-5 flex flex-col justify-center items-center gap-5'>
         <div className='flex gap-10'>
-            <div className='border-3 flex border-2 border-[#d1d1d1] rounded-full w-12 h-12 justify-center items-center'><Instagran/></div>
-            <div className='border-3 flex border-2 border-[#d1d1d1] rounded-full w-12 h-12 justify-center items-center'><Linkedin/></div>
-            <div className='border-3 flex border-2 border-[#d1d1d1] rounded-full w-12 h-12 justify-center items-center'><Whatsapp/></div>
+            <a target='_blank' href='https://www.instagram.com/andyandhunt/' className='border-3 flex border-2 border-[#d1d1d1] rounded-full w-12 h-12 justify-center items-center'><Instagran/></a>
+            <a target='_blank' href='https://www.linkedin.com/in/andr%C3%A9s-cazar-a381a71a9/' className='border-3 flex border-2 border-[#d1d1d1] rounded-full w-12 h-12 justify-center items-center'><Linkedin/></a>
+            <a target='_blank' href='https://walink.co/f6b78d' className='border-3 flex border-2 border-[#d1d1d1] rounded-full w-12 h-12 justify-center items-center'><Whatsapp/></a>
         </div>
         <div className='w-24'>
             <HeaderLogo/>
         </div>
         <hr className="w-5/6 mx-auto max-w-5xl bg-white mb-2"/>
         <div className='flex gap-2 w-full px-2 mx-auto max-w-5xl justify-between items-center'>
-            <div className='flex justify-center items-center'>
+            <a href='/' className='flex justify-center items-center'>
                 <CategoryIcons category='all'/>
                 <p className='text-gray-300 text-sm'>ALL WORK</p>
-            </div>
-            <div className='flex justify-center items-center'>
+            </a>
+            <a href='/design' className='flex justify-center items-center'>
                 <CategoryIcons category='web'/>
                 <p className='text-gray-300 text-sm'>DESIGN</p>
-            </div>
-            <div className='flex justify-center items-center'>
+            </a>
+            <a href='/web' className='flex justify-center items-center'>
                 <CategoryIcons category='design'/>
                 <p className='text-gray-300 text-sm'>WEB</p>
-            </div>
-            <div className='flex justify-center items-center'>
+            </a>
+            <a href='/marketing' className='flex justify-center items-center'>
                 <CategoryIcons category='marketing'/>
                 <p className='text-gray-300 text-sm'>MARKETING</p>
-            </div>
+            </a>
         </div>
         <p className='text-gray-300 pt-5'>cazar.andy@gmail.com</p>
     </div>
